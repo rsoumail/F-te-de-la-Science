92ab43d735a7a02a7f8458db5ffbe7ccd7c76ed6
Soumaila Ramadan <ramadan.soumaila@gmail.com>
2018-02-20 15:01:04 +0100

    Externalise date parser and updates model

--- app/src/main/java/fr/istic/m2il/mmm/fetescience/models/Circuit.java
+++ app/src/main/java/fr/istic/m2il/mmm/fetescience/models/Circuit.java
@@ -1,8 +1,18 @@
 package fr.istic.m2il.mmm.fetescience.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ismael on 29/12/17.
  */
 
 public class Circuit {
+
+    private List<Event> events;
+
+    public Circuit(){
+        events = new ArrayList<>();
+    }
+
 }
